bb9acceb15ebfecd8af1bb98f8306f01aaa1fda1
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-14 10:13:57 +0330

    err point inspector

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e55ba51ae31a32334ae536bc4d6b6867@o4506222819213312.ingest.sentry.io/4506222820982784",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
