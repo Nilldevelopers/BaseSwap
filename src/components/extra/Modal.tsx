4ae3becf4ebeb14ecb3a069db6c49649042ca398
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-19 22:17:44 +0330

    fix modal close button

--- src/components/extra/Modal.tsx
+++ src/components/extra/Modal.tsx
@@ -1,36 +1,44 @@
 import React from 'react';
 
-const Modal = (props: {
-    modalName: string,
-    className?: string,
+interface ModalProps {
+    modalName: string;
+    className?: string;
     showSearchBox?: boolean;
-    children: React.ReactNode
-}) => {
+    children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = (props) => {
     return (
         <>
-            <input type="checkbox" id={props.modalName} className="modal-toggle"/>
+            <input type="checkbox" id={props.modalName} className="modal-toggle" />
             <div className="modal">
                 <div className={`modal-box ${props.className}`}>
                     <div className="flex flex-col gap-[26px]">
-                        <div className="flex flex-row items-center justify-between">
-                            {props.showSearchBox && (
+                        {props.showSearchBox && (
+                            <div className="flex flex-row items-center justify-between">
                                 <input
                                     type="text"
                                     placeholder="Search by name, symbol, or address"
                                     className="input w-full max-w-xs bg-swap-selection-input rounded-[12px]"
                                 />
-                            )}
-                            <div className="modal-action mt-0">
-
-                                <label htmlFor={props.modalName}
-                                       className="btn btn-sm btn-circle btn-ghost">✕</label>
+                                <div className="modal-action mt-0">
+                                    <label htmlFor={props.modalName} className="btn btn-sm btn-circle btn-ghost">
+                                        ✕
+                                    </label>
+                                </div>
                             </div>
-                        </div>
+                        )}
+                        {!props.showSearchBox && (
+                            <div className="modal-action mt-0 flex justify-end">
+                                <label htmlFor={props.modalName} className="btn btn-sm btn-circle btn-ghost">
+                                    ✕
+                                </label>
+                            </div>
+                        )}
 
-                        <React.Fragment>
-                            {props.children}
-                        </React.Fragment>
+                        <React.Fragment>{props.children}</React.Fragment>
                     </div>
+
                 </div>
             </div>
         </>
