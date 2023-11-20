e3e10904dd394aa098b45661b1053c7c358b580f
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-20 23:16:16 +0330

    fix blank page styles imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
-
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss-import');
+const postenv = require('postcss-preset-env');
+const cssnano = require('cssnano');
 
 module.exports = {
-  plugins: {
-    'postcss-preset-env': {},
-    cssnano: process.env.NODE_ENV === 'production' ? {} : false,
-}
+    plugins: [
+        postcss,
+        autoprefixer,
+        tailwindcss,
+        postenv,
+        process.env.NODE_ENV === 'production' ? cssnano : false
+
+    ]
 }
