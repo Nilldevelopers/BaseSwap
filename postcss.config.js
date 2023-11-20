3fc88b6c9344cd9bdc42149884cbcdf8fd687ac0
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-20 22:58:44 +0330

    fix css err

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
+const cssnano = [
+  'cssnano',
+  {
+    preset: 'advanced',
+    discardComments: { removeAll: true }
+  }
+]
+
 module.exports = {
-  plugins: {
-    cssnano: process.env.NODE_ENV === 'production' ? {} : false,
-  },
-};
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [cssnano] : []),
+  ]
+}
