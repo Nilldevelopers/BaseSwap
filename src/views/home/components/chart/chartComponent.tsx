b8bbdf5a6c1c06732e231d43b4428cd4c78abf54
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-22 11:11:42 +0330

    fix swap styles

--- src/views/home/components/chart/chartComponent.tsx
+++ src/views/home/components/chart/chartComponent.tsx
@@ -1,17 +1,9 @@
-import {ColorType, createChart} from 'lightweight-charts';
-import React, {useEffect, useRef} from 'react';
+
+import { createChart, ColorType } from 'lightweight-charts';
+import React, { useEffect, useRef } from 'react';
 
 
-export const ChartComponent = (props: {
-    data: any;
-    colors?: {
-        backgroundColor?: "transparent" | undefined;
-        lineColor?: "#EF233C" | undefined;
-        textColor?: "white" | undefined;
-        areaTopColor?: "rgba(239,35,60,0.48)" | undefined;
-        areaBottomColor?: "rgba(239,35,60,0)" | undefined;
-    } | undefined;
-}) => {
+export const ChartComponent = (props: { data: any; colors?: { backgroundColor?: "transparent" | undefined; lineColor?: "#EF233C" | undefined; textColor?: "white" | undefined; areaTopColor?: "rgba(239,35,60,0.48)" | undefined; areaBottomColor?: "rgba(239,35,60,0)" | undefined; } | undefined; }) => {
     const {
         data,
         colors: {
@@ -27,25 +19,25 @@ export const ChartComponent = (props: {
     useEffect(
         () => {
             const handleResize = () => {
-                chart.applyOptions({width: chartContainerRef.current.clientWidth});
+                chart.applyOptions({ width: chartContainerRef.current.clientWidth });
             };
 
             const chart = createChart(chartContainerRef.current, {
                 layout: {
-                    background: {type: ColorType.Solid, color: backgroundColor},
+                    background: { type: ColorType.Solid, color: backgroundColor },
                     textColor,
                 },
                 grid: {
-                    vertLines: {color: 'transparent'},
-                    horzLines: {color: 'transparent'},
+                    vertLines: { color: 'transparent' },
+                    horzLines: { color: 'transparent' },
                 },
-                width: chartContainerRef.current.clientWidth,
-                height: window.innerHeight > 500 ? 500 : 300,
+                width: chartContainerRef.current.clientWidth ,
+                height: window.innerHeight > 500 ? 500  :  300  ,
 
             });
             chart.timeScale().fitContent();
 
-            const newSeries = chart.addAreaSeries({lineColor, topColor: areaTopColor, bottomColor: areaBottomColor});
+            const newSeries = chart.addAreaSeries({ lineColor, topColor: areaTopColor, bottomColor: areaBottomColor });
             newSeries.setData(data);
 
             window.addEventListener('resize', handleResize);
@@ -59,7 +51,7 @@ export const ChartComponent = (props: {
         [data, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
     );
     useEffect(() => {
-    }, [])
+    } , [])
 
     return (
         <div
@@ -69,20 +61,22 @@ export const ChartComponent = (props: {
 };
 
 const initialData = [
-    {time: '2018-12-22', value: 32.51},
-    {time: '2018-12-23', value: 31.11},
-    {time: '2018-12-24', value: 27.02},
-    {time: '2018-12-25', value: 27.32},
-    {time: '2018-12-26', value: 25.17},
-    {time: '2018-12-27', value: 28.89},
-    {time: '2018-12-28', value: 25.46},
-    {time: '2018-12-29', value: 23.92},
-    {time: '2018-12-30', value: 22.68},
-    {time: '2018-12-31', value: 22.67},
+    { time: '2018-12-22', value: 32.51 },
+    { time: '2018-12-23', value: 31.11 },
+    { time: '2018-12-24', value: 27.02 },
+    { time: '2018-12-25', value: 27.32 },
+    { time: '2018-12-26', value: 25.17 },
+    { time: '2018-12-27', value: 28.89 },
+    { time: '2018-12-28', value: 25.46 },
+    { time: '2018-12-29', value: 23.92 },
+    { time: '2018-12-30', value: 22.68 },
+    { time: '2018-12-31', value: 22.67 },
 ];
 
 export function TVChartContainer() {
     return (
-        <ChartComponent data={initialData}></ChartComponent>
+        <section className="md:w-8/12 w-full md:h-[500px]  mt-3 rounded-xl bg-custom-cart">
+            <ChartComponent data={initialData}></ChartComponent>
+        </section>
     );
 }
