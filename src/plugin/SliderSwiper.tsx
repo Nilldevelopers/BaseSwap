da20e528337d222e1277b94f38e567b63a894a54
mohsen <mohsensalehi2001@gmail.com>
2023-11-27 12:22:39 +0330

    bugfix tutorial

--- src/plugin/SliderSwiper.tsx
+++ src/plugin/SliderSwiper.tsx
@@ -4,9 +4,12 @@ import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import React from "react";
 interface IProps  {
-    elements : React.JSX.Element[]
+    elements : React.JSX.Element[] ,
+    event ?: () => void,
+    htmlFor ?: string ,
+    textBtn : string
 }
-function SliderSwiper({elements} : IProps) {
+function SliderSwiper({elements , event , htmlFor , textBtn} : IProps) {
     return (
         <Swiper
             pagination={{
@@ -19,9 +22,9 @@ function SliderSwiper({elements} : IProps) {
                 <SwiperSlide key={index} className="md:bg-hero-section1 !flex flex-wrap justify-center  items-center">
                     {item}
                     <div className="w-full mb-2 md:m-0  text-center">
-                        <button className="text-[15px] underline p-1 bg-transparent">
-                            Skip Tutorial
-                        </button>
+                        <label onClick={event} htmlFor={htmlFor} className="text-[15px] cursor-pointer underline z-10 p-1 bg-transparent">
+                            {textBtn}
+                        </label>
                     </div>
                 </SwiperSlide>
             ))}
