a76f2a4d943961f2361e112f6abbd5e2e0ae7858
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-29 22:43:55 +0330

    optimized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import HomeView from "@/views/home/HomeView";
 import dynamic from "next/dynamic";
 import chartData from "@/data/chart/ChartData";
 import {NextPage} from "next";
+import {fetchGitHubTokens} from "@/utils/fetchGitHubTokens";
 import {IToken} from "@/interfaces/IToken";
-import {Octokit} from "@octokit/rest";
 
 const WelcomeModal = dynamic(() => import('@/views/home/components/modals/WelcomeModal'));
 
@@ -26,43 +26,24 @@ const Home: NextPage<HomeProps> = ({contractAddress, tokenData}) => {
     );
 }
 
-// export async function getStaticProps() {
 export async function getServerSideProps() {
+    const {tokens} = await fetchGitHubTokens();
+    const response = await fetch(tokens[0].download_url);
 
-    const octokit = new Octokit();
-    const owner = process.env.REPO_OWNER!;
-    const repo = process.env.REPO_NAME!;
-    const contractAddress = process.env.BTC_CONTRACT_ADDRESS!;
-
-    try {
-        const response = await octokit.repos.getContent({
-            owner,
-            repo,
-            path: '/token', // path to the directory, empty for the root
-        });
-
-        if (Array.isArray(response.data)) {
-            const tokens = response.data
-                .filter((item) => item.type === 'file' && item.name.match(/\.(json)$/i))
-                .map((item): any => ({
-                    name: item.name,
-                    download_url: item.download_url,
-                }));
-
-            const tokenData = await tokens[0].download_url.json() as IToken;
-            return {
-                props: {
-                    tokenData,
-                    contractAddress: contractAddress,
-                },
-            };
-        } else {
-            throw new Error('Unexpected response from GitHub API');
-        }
-    } catch (error) {
-        console.error(error);
-        throw new Error('Error fetching data from GitHub API');
+    if (!response.ok) {
+        throw new Error('Failed to fetch data');
     }
+
+    // Parse the JSON data
+    const tokenData = await response.json() as IToken;
+
+    return {
+        props: {
+            tokenData,
+            contractAddress: process.env.BTC_CONTRACT_ADDRESS,
+        },
+    };
+
 }
 
 export default Home
